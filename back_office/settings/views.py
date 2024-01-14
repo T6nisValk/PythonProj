b1dbd0f070c7a2d6e1510c49cb545eb4c565d429
T6nisValk <tnsvalk@gmail.com>
2024-01-14 10:58:13 +0200

    Fixed settings to check if model exists

--- back_office/settings/views.py
+++ back_office/settings/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404
 from .models import Variables
 
 
@@ -9,11 +9,17 @@ def settings(request):
         website_title = request.POST.get("title")
         description = request.POST.get("description")
         currency = request.POST.get("currency")
+
+        if variables is None:
+            variables = Variables()
+
         if website_title:
             variables.website_title = website_title
         if description:
             variables.homepage_description = description
         if currency:
             variables.website_currency = currency
-    variables.save()
+
+        variables.save()
+
     return render(request, "settings.html", {'variables': variables})
